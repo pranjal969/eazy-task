6f95d80e1bac96d82e18e4caebe77670b91a6da5
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2022-08-27 18:30:00 +0530

    Changes in nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
+  // added design 
   return (
     <footer className="bg-[#6f80e2] text-white py-4">
       <div className="container mx-auto text-center">
