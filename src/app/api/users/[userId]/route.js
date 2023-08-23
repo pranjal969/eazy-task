910886c9bfdabc9c5d6431514500ae26962022dd
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-08-23 16:27:58 +0530

    Added Api
    Added Api For delete user by id, get user by id

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,9 +1,31 @@
+import { connectDb } from "@/helper/db";
+import { User } from "@/models/user";
 import { NextResponse } from "next/server";
+connectDb
+//Api for get user by id
+export const GET = async (request, { params }) => {
 
-export function DELETE(request, { params }) {
-    console.log(typeof params.userId);
-    return NextResponse.json({
-        message: "Deleted Successfully",
-        status: "ok"
-    }, { status: 200, statusText: "Deleted" })
+    const { userId } = await params;
+
+    try {
+        const user = await User.findById(userId);
+        return NextResponse.json(user);
+    } catch (error) {
+        return NextResponse.json({ message: "Error getting user", status: "False" }, { status: 200 });
+    }
+
 }
+// delete user by id
+export const DELETE = async (request, { params }) => {
+
+    const { userId } = await params;
+
+    try {
+        const user = await User.deleteOne({_id:userId});
+        return NextResponse.json({ message: "User Deleted successfully", status: "True" }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Error deleting user", status: "False" }, { status: 200 });
+    }
+
+}
+
