a8268adc0734bf2e7566c026040d5ada556d47f4
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-08-24 22:39:57 +0530

    Work update api added
    Work update api added

--- src/app/api/work/[workId]/route.js
+++ src/app/api/work/[workId]/route.js
@@ -1,7 +1,9 @@
 import { connectDb } from "@/helper/db";
 import { Work } from "@/models/work";
 import { NextResponse } from "next/server";
-connectDb();//Api for get work by id
+connectDb();
+
+//Api for get work by id
 
 export const GET = async (request, { params }) => {
 
@@ -12,6 +14,38 @@ export const GET = async (request, { params }) => {
         return NextResponse.json(work || { message: "Work not found", status: "False" });
     } catch (error) {
         return NextResponse.json({ message: "Error getting work", status: "False" }, { status: 200 });
+    }
+
+}
+
+// Update work by id
+export const PUT = async (request, { params }) => {
+
+    const { workId } = await params;
+
+    try {
+        //Get the work details from db
+        const work = await Work.findById(workId);
+        if (work) {
+            // fetch work details from request
+            const {title, description, status, author, category } = await request.json();
+            //create work object with user model
+
+            work.title = title;
+            work.description = description;
+            work.status = status;
+            work.author = author;
+            work.category = category;
+
+            const savedWork = work.save();
+            return NextResponse.json({ message: "Work details updated successfully", status: "True" }, { status: 200 });
+        }
+        else {
+            return NextResponse.json({ message: "Work not found", status: "True" }, { status: 200 });
+        }
+
+    } catch (error) {
+        return NextResponse.json({ message: "Error updating work ", status: "False" }, { status: 200 });
     }
 
 }
