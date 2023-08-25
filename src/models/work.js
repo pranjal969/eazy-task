5d1b42921dd51dc2aab839c9271b985326afd418
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-08-25 21:35:53 +0530

    Added currentDate
    Added currentDate on status_enum.

--- src/models/work.js
+++ src/models/work.js
@@ -1,18 +1,24 @@
 const { Schema, default: mongoose } = require("mongoose");
+const STATUS_ENUM = ['Pending', 'Completed'];
 
-const workSchema=new Schema({
-title:String,
-description:{
-    type:String,
-    required:[true,"Title Required !!"],
-},
-status:{
-    type:String,
-    required:true,
-},
-author:String,
-category:String
+const workSchema = new Schema({
+    title: String,
+    description: {
+        type: String,
+        required: [true, "Title Required !!"],
+    },
+    status: {
+        type: String,
+        enum: STATUS_ENUM,
+        default: 'Pending',
+    },
+    author: String,
+    category: String,
+    addedDate: {
+        type: Date,
+        default: Date.now,
+    }
 });
 
-export const Work=mongoose.models.work || mongoose.model("work",workSchema)
+export const Work = mongoose.models.work || mongoose.model("work", workSchema)
 
