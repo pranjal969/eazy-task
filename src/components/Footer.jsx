17410597ceb0b7c1d8fd31c80c8e6e6cd29416b2
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-08-27 18:30:00 +0530

    Changes in nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
+    // changes in nav 
     <footer className="bg-[#6f80e2] text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2023 EazY Task. All rights reserved.</p>
