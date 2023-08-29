8f728c7aa2db431883ca7681c4835d2d3a43e204
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-08-29 18:30:00 +0530

    Add  tasks changes

--- src/app/add-task/page.js
+++ src/app/add-task/page.js
@@ -1,9 +1,106 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import React, { useState } from 'react';
+import { TextField, Grid, Button } from '@mui/material';
+
+const initialFormData = {
+  title: 'My Task',
+  description: 'Complete my task by 8 pm',
+  status: 'Completed',
+  author: 'Pranjal',
+  category: 'N/A',
+  userId: '64e5dd57f067a1cfd91e09c1',
+};
+
+const AddTaskPage = () => {
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+    console.log(formData);
+  };
+
   return (
-    <div>Add task</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <form onSubmit={handleSubmit} className="w-full max-w-4xl p-4 bg-white rounded shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Add Task</h2>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Title"
+              name="title"
+              fullWidth
+              value={formData.title}
+              onChange={handleChange}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Description"
+              name="description"
+              fullWidth
+              value={formData.description}
+              onChange={handleChange}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Status"
+              name="status"
+              fullWidth
+              value={formData.status}
+              onChange={handleChange}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Author"
+              name="author"
+              fullWidth
+              value={formData.author}
+              onChange={handleChange}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Category"
+              name="category"
+              fullWidth
+              value={formData.category}
+              onChange={handleChange}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="User ID"
+              name="userId"
+              fullWidth
+              value={formData.userId}
+              onChange={handleChange}
+              margin="normal"
+            />
+          </Grid>
+        </Grid>
+        <Button  className="bg-blue-500 hover:bg-blue-600 text-white" type="submit" variant="contained" color="primary" fullWidth>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
 
-export default page
+export default AddTaskPage;
