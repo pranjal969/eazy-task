27a8c30448b88359cb8373c27debf1c1fff7f2f8
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-08-30 18:30:00 +0530

    Add  tasks changes

--- src/app/add-task/page.js
+++ src/app/add-task/page.js
@@ -4,11 +4,11 @@ import React, { useState } from 'react';
 import { TextField, Grid, Button, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
 
 const initialFormData = {
-  title: 'My Task',
-  description: 'Complete my task by 8 pm',
-  status: 'Select Status',
-  author: 'Pranjal',
-  category: 'N/A',
+  title: '',
+  description: '',
+  status: '',
+  author: '',
+  category: '',
   userId: '64e5dd57f067a1cfd91e09c1',
 };
 
@@ -26,89 +26,92 @@ const AddTaskPage = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission here
+    // Add validations over here
     console.log(formData);
   };
 
   return (
-    <div className="flex justify-center items-center mt-5">
-      <form onSubmit={handleSubmit} className="w-full max-w-4xl p-4 bg-white rounded shadow-md">
-        <div className='flex justify-center'>
-          <img src="/task.png" width={"100px"} height={"100px"} alt="" />
-        </div>
-        <h2 className="text-2xl font-semibold mb-4 flex justify-center">Add Task</h2>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              label="Title"
-              name="title"
-              fullWidth
-              value={formData.title}
-              onChange={handleChange}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Description"
-              name="description"
-              fullWidth
-              value={formData.description}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid className='mt-4' item xs={6}>
-            <FormControl fullWidth>
-              <InputLabel>Status</InputLabel>
-              <Select
-                label="Status"
-                name="status"
-                value={formData.status}
+    <div className='fluid-container'>
+      <div className="flex justify-center items-center mt-5">
+        <form onSubmit={handleSubmit} className="w-full max-w-4xl p-4 bg-white rounded shadow-md">
+          <div className='flex justify-center'>
+            <img src="/task.png" width={"100px"} height={"100px"} alt="" />
+          </div>
+          <h2 className="text-2xl font-semibold mb-4 flex justify-center">Add Task</h2>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                label="Title"
+                name="title"
+                fullWidth
+                value={formData.title}
                 onChange={handleChange}
-              >
-                <MenuItem value="Select Status">Select Status</MenuItem>
-                <MenuItem value="Completed">Completed</MenuItem>
-                <MenuItem value="Pending">Pending</MenuItem>
-              </Select>
-            </FormControl>
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Description"
+                name="description"
+                fullWidth
+                value={formData.description}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid className='mt-4' item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel>Status</InputLabel>
+                <Select
+                  label="Status"
+                  name="status"
+                  value={formData.status}
+                  onChange={handleChange}
+                >
+                  <MenuItem value="Select Status" disabled>Select Status</MenuItem>
+                  <MenuItem value="Completed">Completed</MenuItem>
+                  <MenuItem value="Pending">Pending</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Author"
+                name="author"
+                fullWidth
+                value={formData.author}
+                onChange={handleChange}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Category"
+                name="category"
+                fullWidth
+                value={formData.category}
+                onChange={handleChange}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="User ID"
+                name="userId"
+                fullWidth
+                value={formData.userId}
+                onChange={handleChange}
+                margin="normal"
+                disabled
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Author"
-              name="author"
-              fullWidth
-              value={formData.author}
-              onChange={handleChange}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Category"
-              name="category"
-              fullWidth
-              value={formData.category}
-              onChange={handleChange}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="User ID"
-              name="userId"
-              fullWidth
-              value={formData.userId}
-              onChange={handleChange}
-              margin="normal"
-              disabled
-            />
-          </Grid>
-        </Grid>
-        <div className='mt-2 mb-8'>
-          <Button className="bg-blue-500 hover:bg-blue-600 text-white " type="submit" variant="contained" color="primary" fullWidth>
-            Submit
-          </Button>
-        </div>
-      </form>
+          <div className='mt-2 mb-8'>
+            <Button className="bg-blue-500 hover:bg-blue-600 text-white " type="submit" variant="contained" color="primary" fullWidth>
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
