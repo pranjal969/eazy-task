933941aa5ad1323a6a2d939b83e7310a02f35f5a
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-08-31 18:30:00 +0530

    Add banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/homepage/Banner";
+import Features from "@/components/homepage/Features";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Features />
     </div>
   )
 }
---
+++ src/components/homepage/Features.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FaTasks, FaClock, FaCheckCircle } from 'react-icons/fa';
+
+const Features = () => {
+  return (
+    <section className="bg-[#a5b4fc] py-16"> {/* Set your desired background color */}
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-semibold mb-6 text-white">Key Features</h2> {/* Set text color to white */}
+        <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-12">
+          {/* Feature 1 */}
+          <div className="flex items-center space-x-4">
+            <div className="rounded-full bg-blue-500 p-3 text-white">
+              <FaTasks className="text-2xl" />
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold">Task Management</h3>
+              <p className="text-gray-600">
+                Easily manage your tasks and stay organized.
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 2 */}
+          <div className="flex items-center space-x-4">
+            <div className="rounded-full bg-blue-500 p-3 text-white">
+              <FaClock className="text-2xl" />
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold">Set Due Dates</h3>
+              <p className="text-gray-600">
+                Set due dates to keep track of deadlines.
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 3 */}
+          <div className="flex items-center space-x-4">
+            <div className="rounded-full bg-blue-500 p-3 text-white">
+              <FaCheckCircle className="text-2xl" />
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold">Task Completion</h3>
+              <p className="text-gray-600">
+                Mark tasks as complete and track your progress.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
