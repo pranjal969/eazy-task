6b50e83382407768a6441c83e267d56d22e64db0
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-09-04 18:30:00 +0530

    Jwt tokrn  done

---
+++ src/app/api/login/route.js
@@ -0,0 +1,52 @@
+import { connectDb } from "@/helper/db";
+import { User } from "@/models/user";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"; 
+
+connectDb();
+
+// Secret key for JWT
+const jwtSecret = process.env.JWT_SECRET;
+
+// Api for login
+export const POST = async (request) => {
+    const saltRounds = parseInt(process.env.SALT_ROUNDS);
+    const salt = process.env.PASSWORD_SALT;
+    try {
+        // fetch user details from request
+        const { email, password } = await request.json();
+        const user = await User.findOne({
+            email: email,
+        });
+        // Combine the custom salt with the password
+        const saltedPassword = `${salt}${password}`;
+        // Hash the salted password using bcrypt
+        const hashedPassword = await bcrypt.hash(saltedPassword, saltRounds);
+        if (user) {
+            const matchedPassword = bcrypt.compareSync(saltedPassword, user.password);
+            if (!matchedPassword) {
+                throw new Error("Password Incorrect");
+            }
+
+            //Else Generate a JWT token
+            const token = jwt.sign(
+                {
+                    userId: user._id,
+                    name:user.name, 
+                    email:user.email, // Include any user-related data you need
+                },
+                jwtSecret,
+                { expiresIn: '1h' } // Set an expiration time for the token
+            );
+
+            // Send the JWT token in the response
+            return NextResponse.json({token}, { status: 200, statusText: "ok" });
+        } else {
+            throw new Error("User Not Found");
+        }
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: error.message, status: "False" }, { status: 400 });
+    }
+};
