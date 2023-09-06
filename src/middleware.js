fe531225f89f91a04948265c4169a6ec3e0962c9
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-09-06 13:41:46 +0530

    changes in cookies
    changes in cookies

--- src/middleware.js
+++ src/middleware.js
@@ -2,29 +2,40 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
   const authToken = await request.cookies.get("authToken")?.value;
+  const path = await request.nextUrl.pathname;
+  const accessAfterLoginUrls = ['/add-task', '/profile'];
+  const NoAccessAfterLoginUrls = ['/signup', '/login'];
+  switch (true) {
+    case path.startsWith('/api'):
+      if (!authToken) {
+        return NextResponse.json(
+          {
+            message: "Unauthorized access prohibited",
+            securedVia: "256 Bit Encryption",
+            status: false
+          },
+          { status: 401 }
+        );
+      }
+      break;
 
-  // Check if the request is for an API route
-  if (request.nextUrl.pathname.startsWith('/api')) {
-    // If it's an API route, check for authToken
-    if (!authToken) {
-      // If authToken is not present for API routes, deny access
-      return NextResponse.json({ message: "Unauthorized access prohibted", status: "False", securedVia: "AWS_Token" }, { status: 401 });
-    }
-  } else if (request.nextUrl.pathname.startsWith('/signup') ||
-    request.nextUrl.pathname.startsWith('/login')) {
-    if (authToken) {
-      // If authToken is present, redirect to the profile page
-      return NextResponse.redirect(new URL('/profile', request.url));
-    }
-    else {
-      return;
-    }
+    case NoAccessAfterLoginUrls.includes(path):
+      if (authToken) {
+        return NextResponse.redirect(new URL('/profile', request.url));
+      }
+      break;
+
+    case accessAfterLoginUrls.includes(path):
+      if (!authToken) {
+        return NextResponse.redirect(new URL('/login', request.url));
+      }
+      break;
+
+    default:
+      break;
   }
-
-  return;
 }
 
-// Match all routes under /api
 export const config = {
-  matcher: ['/signup', '/login', '/add-task', '/api/users:path*','/api/work:path*'],
-}
+  matcher: ['/signup', '/login', '/profile', '/add-task', '/api/users/:path*', '/api/work/:path*'],
+};
