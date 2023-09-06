de9b4950f456a0c89a93bd9db84d422f54327524
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-09-06 12:00:26 +0530

    Updated middleware
    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,30 @@
-import { NextResponse } from 'next/server'
-
+import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
-
   const authToken = await request.cookies.get("authToken")?.value;
-  console.log(authToken);
-  return NextResponse.redirect(new URL('/', request.url))
+
+  // Check if the request is for an API route
+  if (request.nextUrl.pathname.startsWith('/api')) {
+    // If it's an API route, check for authToken
+    if (!authToken) {
+      // If authToken is not present for API routes, deny access
+      return NextResponse.json({ message: "Unauthorized access prohibted", status: "False", securedVia: "AWS_Token" }, { status: 401 });
+    }
+  } else if (request.nextUrl.pathname.startsWith('/signup') ||
+    request.nextUrl.pathname.startsWith('/login')) {
+    if (authToken) {
+      // If authToken is present, redirect to the profile page
+      return NextResponse.redirect(new URL('/profile', request.url));
+    }
+    else {
+      return;
+    }
+  }
+
+  return;
 }
 
-// See "Matching Paths" 
+// Match all routes under /api
 export const config = {
-  matcher: ['/signup', '/login', '/add-task',],
+  matcher: ['/signup', '/login', '/add-task', '/api/users:path*','/api/work:path*'],
 }
