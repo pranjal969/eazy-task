7ec2b2f9c7283cfe7a43d5d768852c60a78979a1
Pranjal Kumar <102158568+pranjal969@users.noreply.github.com>
2023-09-13 18:30:00 +0530

    Added responsive

--- src/components/NoTasksMessage.jsx
+++ src/components/NoTasksMessage.jsx
@@ -11,16 +11,18 @@ const NoTasksMessage = () => {
 
   return (
     <div className="text-center">
-<div >        <div className="bg-white shadow-md rounded-lg p-6">
-        <p className="text-xl font-semibold mb-4">No task added</p>
-        <p className="text-lg mb-4">Click the button below to add a new task.</p>
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-          onClick={handleAddTaskClick}
-        >
-          Add Task
-        </button>
-      </div></div>
+      <div className="mx-2 md:mx-8 lg:mx-20 pt-20 md:pt-40">
+        <div className="bg-white shadow-md rounded-lg p-6">
+          <p className="text-xl md:text-2xl lg:text-3xl font-semibold mb-4">No task added</p>
+          <p className="text-lg md:text-xl lg:text-2xl mb-4">Click the button below to add a new task.</p>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 text-lg md:text-xl lg:text-2xl"
+            onClick={handleAddTaskClick}
+          >
+            Add Task
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
